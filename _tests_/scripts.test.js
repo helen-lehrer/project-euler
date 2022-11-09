0140df92636205d659a94892188290c7a4b29f02
Helen Lehrer <hlehrer@outlook.com>
2022-11-08 17:02:35 -0800

    Add problem 2 & 3

--- _tests_/scripts.test.js
+++ _tests_/scripts.test.js
@@ -1,7 +1,21 @@
 import { findMult } from './../src/js/scripts.js';
+import { findFibonacciEvens } from './../src/js/scripts.js';
+import { largestPrimeFactor } from './../src/js/scripts.js';
 
 describe('findMult', () => {
 	test('should find sum of multiples to a specified limit', () => {
 		expect(findMult(1000)(3)(5)).toEqual(233168)
+	})
+})
+
+describe('findFibonacciEvens', () => {
+	test('Problem 2:  should find sum of even-valued fibonacci nums to a specified limit', () => {
+		expect(findFibonacciEvens(4000000)(2)).toEqual(4613730);
+	})
+})
+
+describe('largestPrimeFactor', () => {
+	test('should find largest prime factor', () => {
+		expect(largestPrimeFactor(600851475143)).toEqual();
 	})
 })
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,5 @@
+/*  Full list of 'Project Euler' problems: https://projecteuler.net/archives  */
+//Problem #1 
 export const findMult = (limit) => {
 	let sum = 0;
 	return (multiple) =>{
@@ -12,3 +14,63 @@ export const findMult = (limit) => {
 		}
 	}
 }
+
+//Problem #2 (Solved by:  Sam Majerus). 
+export const findFibonacciEvens = (limit) => {
+	let sum = 0;
+	let limitReached = 1; 
+	let currentNum = 0; 
+	let prevNums = [1, 2];
+	return (multiple) =>{
+		let i = 1; 
+		while(limitReached != 0)
+		{
+			currentNum = prevNums[i] + prevNums[i-1];
+			if(currentNum >= limit){
+				limitReached=0; 
+			}else{
+				prevNums[i+1] = currentNum; 
+				console.log("Current array: " + prevNums + ".    \n CurrentNum right now is: " + currentNum);
+				if (currentNum % multiple === 0){
+					sum += currentNum;
+				}
+				i++;
+			} 
+		}
+		return sum;
+	}
+}
+
+export const largestPrimeFactor = (n) => {
+let divisor=2;
+while (divisor <= n)
+{
+  if (n%divisor == 0){
+   n /= divisor
+  } else {
+    divisor++;
+  }
+}
+return divisor; 
+}
+
+
+// export const largestPrimeFactor = (n) => {
+//   let i=2;
+// 	let primeFactor = 0;
+//   while (i <= n)
+//   {
+//     if (n%i == 0){
+//       for(let j=2; j < n; j++)
+// 			{
+// 				if (!i%j ==0)
+// 				{
+// 					primeFactor = i
+// 				}
+// 			}
+//     } else {
+//       i++;
+//     }
+//   }
+//   return primeFactor; 
+// }
